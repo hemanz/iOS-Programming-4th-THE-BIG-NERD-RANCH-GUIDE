ae9974994c10af879f08df2cfcab10c81b2905d0
3738799heman <wataxiwaheman@gmail.com>
2015-12-19 11:10:10 +0800

    循环的圆圈

--- 04Views and the View Hierarchy/Hypnosister/Hypnosister/HypnosisterView.m
+++ 04Views and the View Hierarchy/Hypnosister/Hypnosister/HypnosisterView.m
@@ -16,17 +16,20 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
-    float radius = MIN(bounds.size.width, bounds.size.height) / 2.0;
+    float maxRidus = hypot(bounds.size.width, bounds.size.height) / 2.0;
     
-    UIBezierPath * path = [[UIBezierPath alloc] init];
-    [path addArcWithCenter:center
-                    radius:radius
-                startAngle:0.0
-                  endAngle:M_PI*2.0
-                 clockwise:YES];
-    path.lineWidth = 10;
-    [[UIColor greenColor] setStroke];
-    [path stroke];
+    
+    for (float currentRadius = maxRidus; currentRadius > 0; currentRadius-=20.0) {
+        UIBezierPath * path = [[UIBezierPath alloc] init];
+        [path addArcWithCenter:center
+                        radius:currentRadius
+                    startAngle:0.0
+                      endAngle:M_PI*2.0
+                     clockwise:YES];
+        path.lineWidth = 10;
+        [[UIColor greenColor] setStroke];
+        [path stroke];
+    }
     
 }
 
